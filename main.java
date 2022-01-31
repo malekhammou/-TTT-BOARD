f51e423ec8f4cb67375a3ca6bfce4f2a3dc10c92
Malek Hammou <46074100+malekhammou@users.noreply.github.com>
2022-02-01 00:31:48 +0100

    Add files via upload
    Main file

---
+++ main.java
@@ -0,0 +1,75 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import javax.swing.text.html.ImageView;
+import javax.swing.Icon;
+import javax.swing.text.Element;
+
+
+public class main {
+
+    public static void main(String[] args) {
+    
+        /*NEW FRAME*/
+        JFrame mainFrame=new JFrame();
+        mainFrame.setTitle("TIC TAC TOE BOARD");
+        mainFrame.setSize(500,600);
+        mainFrame.setLayout(new BorderLayout());
+        JPanel digitsPanel=new JPanel();
+        digitsPanel.setLayout(new GridLayout(3, 3));
+        for (int i = 1; i < 10; i++) {
+            JLabel label=new JLabel("");
+            label.setHorizontalAlignment(SwingConstants.CENTER);
+            Border blackline = BorderFactory.createLineBorder(Color.black);
+            label.setFont(new Font("Serif", Font.BOLD, 55));
+           label.setForeground(Color.black);
+
+            label.setBorder(blackline);
+            digitsPanel.add(label);
+            label.addMouseListener(new MouseAdapter() {
+
+        @Override
+
+        public void mouseClicked(MouseEvent e) {
+         long test = Math.round( Math.random());
+         if(test==0){
+                      label.setText("X");
+
+         }
+         else{
+                      label.setText("O");
+
+         }
+         
+    //label.setIcon(new javax.swing.ImageIcon(getClass().getResource("D:/tictactoe/src/main/java/icons/O.png")));
+   
+
+   }
+
+    });
+     
+        mainFrame.add(digitsPanel,BorderLayout.CENTER);
+        mainFrame.setVisible(true);
+
+    }
+
+    }}
